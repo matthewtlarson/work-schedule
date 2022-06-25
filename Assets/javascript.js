47077c16c33712fc0f1c59efe4cca87d697d6849
Matthew Larson <matthewtlarson@email.arizona.edu>
2022-06-24 20:39:00 -0700

    final save

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -2,47 +2,47 @@ var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm a"));
 
 $(document).ready(function () {
-  $(".saveBtn").on("click", function () {
-      var text = $(this).siblings(".description").val();
-      var time = $(this).parent().attr("id");
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-      localStorage.setItem(time, text);
-  })
+        localStorage.setItem(time, text);
+    })
 
-  function timeTracker() {
-      var timeNow = moment().hour();
+    function timeTracker() {
+        var timeNow = moment().hour();
 
-      $(".time-block").each(function () {
-          var blockTime = parseInt($(this).attr("id").split("")[1]);
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("")[1]);
 
-          if (blockTime < timeNow) {
-              $(this).removeClass("future");
-              $(this).removeClass("present");
-              $(this).addClass("past");
-          }
-          else if (blockTime === timeNow) {
-              $(this).removeClass("past");
-              $(this).removeClass("future");
-              $(this).addClass("present");
-          }
-          else {
-              $(this).removeClass("present");
-              $(this).removeClass("past");
-              $(this).addClass("future");
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
 
-          }
-      })
-  }
+            }
+        })
+    }
 
-  $("#9 AM .description").val(localStorage.getItem("9 AM"));
-  $("#10 AM .description").val(localStorage.getItem("10 AM"));
-  $("#11 AM .description").val(localStorage.getItem("11 AM"));
-  $("#12 PM .description").val(localStorage.getItem("12 PM"));
-  $("#1 PM .description").val(localStorage.getItem("1 PM"));
-  $("#2 PM .description").val(localStorage.getItem("2 PM"));
-  $("#3 PM .description").val(localStorage.getItem("3 PM"));
-  $("#4 PM .description").val(localStorage.getItem("4 PM"));
-  $("#5 PM .description").val(localStorage.getItem("5 PM"));
+    $("#9 AM .description").val(localStorage.getItem("9 AM"));
+    $("#10 AM .description").val(localStorage.getItem("10 AM"));
+    $("#11 AM .description").val(localStorage.getItem("11 AM"));
+    $("#12 PM .description").val(localStorage.getItem("12 PM"));
+    $("#1 PM .description").val(localStorage.getItem("1 PM"));
+    $("#2 PM .description").val(localStorage.getItem("2 PM"));
+    $("#3 PM .description").val(localStorage.getItem("3 PM"));
+    $("#4 PM .description").val(localStorage.getItem("4 PM"));
+    $("#5 PM .description").val(localStorage.getItem("5 PM"));
 
-  timeTracker();
+    timeTracker();
 })
--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # Work Day Scheduler Starter Code
-//GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with time blocks for standard business hours
-WHEN I view the time blocks for that day
-THEN each time block is color-coded to indicate whether it is in the past, present, or future
-WHEN I click into a time block
-THEN I can enter an event
-WHEN I click the save button for that time block
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
+First I began with the HTML skeleton;
+Then I linked the necessary stylesheets for fonts as well as my CSS;
+Next I created titles as well as headings;
+Then I made a lot of divs all with the same classes to maintain organization;
+Within the divs I added all the necessary buttons in addition to its class/styles;
+Next I made a footer to make the format a little more how I wanted it;
+Then I linked my scripts at the bottom;
+Next I started my styling and used the classes I created on my HTML page to link specifically where I wanted them;
+After I began the javascript and started by outting a current date below the header;
+Then I save button to record the response and input it to local storage;
+Finally, I created a function to highlight the past dates, current dates, and future dates, which does not work.
+<link href="file:///C:/Users/matth/Documents/class-stuff/work-schedule/index.html"/>
