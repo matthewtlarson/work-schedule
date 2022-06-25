6d05fe46a06ffc18896d6e107d8003a5eae62569
Matthew Larson <matthewtlarson@email.arizona.edu>
2022-06-24 17:35:39 -0700

    save

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
       var timeNow = moment().hour();
 
       $(".time-block").each(function () {
-          var blockTime = parseInt($(this).attr("id").split("")[4]);
+          var blockTime = parseInt($(this).attr("id").split("")[1]);
 
           if (blockTime < timeNow) {
               $(this).removeClass("future");
@@ -34,15 +34,15 @@ $(document).ready(function () {
       })
   }
 
-  $("#9AM .description").val(localStorage.getItem("9AM"));
-  $("#10AM .description").val(localStorage.getItem("10AM"));
-  $("#11AM .description").val(localStorage.getItem("11AM"));
-  $("#12PM .description").val(localStorage.getItem("12PM"));
-  $("#1PM .description").val(localStorage.getItem("1PM"));
-  $("#2PM .description").val(localStorage.getItem("2PM"));
-  $("#3PM .description").val(localStorage.getItem("3PM"));
-  $("#4PM .description").val(localStorage.getItem("4PM"));
-  $("#5PM .description").val(localStorage.getItem("5PM"));
+  $("#9 AM .description").val(localStorage.getItem("9 AM"));
+  $("#10 AM .description").val(localStorage.getItem("10 AM"));
+  $("#11 AM .description").val(localStorage.getItem("11 AM"));
+  $("#12 PM .description").val(localStorage.getItem("12 PM"));
+  $("#1 PM .description").val(localStorage.getItem("1 PM"));
+  $("#2 PM .description").val(localStorage.getItem("2 PM"));
+  $("#3 PM .description").val(localStorage.getItem("3 PM"));
+  $("#4 PM .description").val(localStorage.getItem("4 PM"));
+  $("#5 PM .description").val(localStorage.getItem("5 PM"));
 
   timeTracker();
 })
